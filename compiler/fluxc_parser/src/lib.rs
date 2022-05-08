de8acf907cd510ae36413717285b3e5f66166b74
kaylendog <actuallyori@gmail.com>
2022-05-08 11:30:26 +0100

    chore(parser): move bin op precedence parser to bin_expr.rs

--- compiler/fluxc_parser/src/lib.rs
+++ compiler/fluxc_parser/src/lib.rs
@@ -2,64 +2,14 @@
 
 use fluxc_ast::{Ident, Node, Stmt, AST};
 use fluxc_errors::CompilerError;
-use lazy_static::lazy_static;
 use pest::{
     error::{Error, ErrorVariant},
     iterators::Pair,
-    prec_climber::{Assoc, Operator, PrecClimber},
     Parser, Span,
 };
 
 mod expr;
 mod stmt;
-
-lazy_static! {
-    /// The precedence climber for parsing binary expressions. Since binary expressions are recursive, and the precedence
-    /// of operators cannot easily be inferred, we use the PrecClimber to ensure that the parser grammar will not left recurse.
-    /// This has the added benefit of handling operator precedence and associativity properly.
-    static ref BIN_EXP_CLIMBER: PrecClimber<Rule> = PrecClimber::new(vec![
-        // 15
-        Operator::new(Rule::binary_op_assign, Assoc::Left) |
-        Operator::new(Rule::binary_op_mul_eq, Assoc::Left) |
-        Operator::new(Rule::binary_op_div_eq, Assoc::Left) |
-        Operator::new(Rule::binary_op_mod_eq, Assoc::Left) |
-        Operator::new(Rule::binary_op_plus_eq, Assoc::Left) |
-        Operator::new(Rule::binary_op_minus_eq, Assoc::Left) |
-        Operator::new(Rule::binary_op_lshift_eq, Assoc::Left) |
-        Operator::new(Rule::binary_op_rshift_eq, Assoc::Left) |
-        Operator::new(Rule::binary_op_bitwise_and_eq, Assoc::Left) |
-        Operator::new(Rule::binary_op_bitwise_or_eq, Assoc::Left) |
-        Operator::new(Rule::binary_op_bitwise_xor_eq, Assoc::Left),
-        // 14
-        Operator::new(Rule::binary_op_logical_or, Assoc::Right),
-        // 13
-        Operator::new(Rule::binary_op_logical_and, Assoc::Right),
-        // 12
-        Operator::new(Rule::binary_op_eq, Assoc::Right) |
-            Operator::new(Rule::binary_op_ne, Assoc::Right),
-        // 11
-        Operator::new(Rule::binary_op_lt, Assoc::Right) |
-            Operator::new(Rule::binary_op_gt, Assoc::Right) |
-            Operator::new(Rule::binary_op_le, Assoc::Right) |
-            Operator::new(Rule::binary_op_ge, Assoc::Right),
-        // 10
-        Operator::new(Rule::binary_op_bitwise_or, Assoc::Right),
-        // 9
-        Operator::new(Rule::binary_op_bitwise_xor, Assoc::Right),
-        // 8
-        Operator::new(Rule::binary_op_bitwise_and, Assoc::Right),
-        // 7
-        Operator::new(Rule::binary_op_lshift, Assoc::Right) |
-            Operator::new(Rule::binary_op_rshift, Assoc::Right),
-        // 6
-        Operator::new(Rule::binary_op_plus, Assoc::Right)
-            | Operator::new(Rule::binary_op_minus, Assoc::Right),
-        // 5
-        Operator::new(Rule::binary_op_mul, Assoc::Right)
-            | Operator::new(Rule::binary_op_div, Assoc::Right)
-            | Operator::new(Rule::binary_op_mod, Assoc::Right)
-    ]);
-}
 
 /// Internal moduel to prevent leakage of the `Rule` type to external
 /// crates.
