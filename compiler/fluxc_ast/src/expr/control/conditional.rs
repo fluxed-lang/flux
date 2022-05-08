df3ed5653f7492099ab6c7e0c17a4f371f0757f1
kaylendog <actuallyori@gmail.com>
2022-05-08 12:27:16 +0100

    chore(parser): implement Conditional parsing

--- compiler/fluxc_ast/src/expr/control/conditional.rs
+++ compiler/fluxc_ast/src/expr/control/conditional.rs
@@ -4,11 +4,20 @@ use crate::{Block, Expr, Node};
 #[derive(Debug, PartialEq)]
 pub struct Conditional {
     /// The opening `if` statement of this conditional.
-    pub if_stmt: (Box<Expr>, Node<Block>),
+    pub if_stmt: Node<IfStmt>,
     /// A list of `else if` statements to test if the above conditional is not
     /// executed.
-    pub else_ifs: Vec<(Expr, Node<Block>)>,
+    pub else_ifs: Vec<Node<IfStmt>>,
     /// An optional `else` block to execute if all above conditionals are not
     /// executed.
-    pub else_stmt: Option<Block>,
+    pub else_stmt: Option<Node<Block>>,
+}
+
+/// A conditional `if` statement.
+#[derive(Debug, PartialEq)]
+pub struct IfStmt {
+	/// The condition to test.
+	pub condition: Box<Node<Expr>>,
+	/// The value to return if the condition is true.
+	pub value: Node<Block>,
 }
