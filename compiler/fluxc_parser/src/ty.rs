b19c6ce4fbae4964107aecdca63845a986721e11
kaylendog <actuallyori@gmail.com>
2022-05-28 16:44:29 +0100

    feat(parser): add Parse impl for Type

--- compiler/fluxc_parser/src/ty.rs
+++ compiler/fluxc_parser/src/ty.rs
@@ -1,15 +1,21 @@
 use fluxc_ast::Node;
 use fluxc_errors::CompilerError;
-use fluxc_types::Type;
+use fluxc_types::{Primitive, Type};
 use pest::iterators::Pair;
 
-use crate::{Context, Parse};
+use crate::{Context, Parse, Rule};
+
+impl Parse for Primitive {
+    fn parse<'i>(input: Pair<'i, Rule>, ctx: &mut Context) -> Result<Node<Self>, CompilerError> {}
+}
 
 impl Parse for Type {
-    fn parse<'i>(
-        input: Pair<'i, crate::parser::Rule>,
-        ctx: &mut Context,
-    ) -> Result<Node<Self>, CompilerError> {
-        todo!()
+    fn parse<'i>(input: Pair<'i, Rule>, ctx: &mut Context) -> Result<Node<Self>, CompilerError> {
+        let node = ctx.new_empty(input.as_span());
+        let inner = input.into_inner().next().unwrap();
+
+        Ok(match inner.as_rule() {
+            Rule::type_literal => node.fill(Type::Primitive(Primitive::parse(inner, ctx)?)),
+        })
     }
 }
